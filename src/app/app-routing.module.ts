24fa6cbc0c93684198b8b43828c7f9b451274541
lalasp18 <laspio18@gmail.com>
2023-09-09 13:16:26 -0300

    2 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AtorComponent } from './s#1/criar/ator/ator.component';
+import { ClasseComponent } from './s#1/criar/classe/classe.component';
+import { DiretorComponent } from './s#1/criar/diretor/diretor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'ator-control', component: AtorComponent },
+  { path: 'classe-control', component: ClasseComponent },
+  { path: 'diretor-control', component: DiretorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+import { AtorComponent } from './s#1/criar/ator/ator.component';
+import { ClasseComponent } from './s#1/criar/classe/classe.component';
+import { DiretorComponent } from './s#1/criar/diretor/diretor.component';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AtorComponent,
+    ClasseComponent,
+    DiretorComponent,
+    NavHeaderComponent,
+    FooterComponent,
+    HomeComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
