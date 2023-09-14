bb94ad6294a549cb549730d86f9d05d042447688
IsabellaPassosCorrea <120729807+IsabellaPassosCorrea@users.noreply.github.com>
2023-09-14 16:19:10 -0300

    up classe e diretor (edit)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,14 +16,13 @@ import { AlertService } from './_services/alert.service';
 import { ClasseModule } from './sprints/s1/criar/classe/routing/classe.module';
 import { DiretorModule } from './sprints/s1/criar/diretor/routing/diretor.module';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
     HomeComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sprints/s1/editar/diretor/diretor-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretor',
+  templateUrl: './diretor-edit.component.html',
+  styleUrls: ['./diretor-edit.component.scss']
+})
+export class DiretorComponent {
+
+}
