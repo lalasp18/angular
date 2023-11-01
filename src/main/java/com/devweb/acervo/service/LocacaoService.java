64bf1535f78b83a982c5f9e1b80540daa0eca43f
IsabellaPassosCorrea <120729807+IsabellaPassosCorrea@users.noreply.github.com>
2023-11-01 16:35:25 -0300

    create: locadora (model,repository,service and controller)

---
+++ src/main/java/com/devweb/acervo/service/LocacaoService.java
@@ -0,0 +1,58 @@
+package com.devweb.acervo.service;
+
+import java.util.List;
+
+import javax.management.relation.RelationTypeNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devweb.acervo.model.Locacao;
+import com.devweb.acervo.repository.LocacaoRepository;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Service
+@Tag(name = "LocacaoService", description = "Fornece serviços web REST para acesso e manipulação de dados de Locacao.")
+public class LocacaoService {
+
+    @Autowired
+    private LocacaoRepository locacaoRepo;
+
+    public Locacao saveAll(Locacao locacao) {
+        return locacaoRepo.save(locacao);
+    }
+
+    public Locacao editAll(Locacao locacao) throws RelationTypeNotFoundException {
+        Locacao editado = locacaoRepo.findById(locacao.getIdLocacao())
+                .orElseThrow(
+                        () -> new RelationTypeNotFoundException(
+                                "Locação não existe com id :" + locacao.getIdLocacao()));
+
+        editado.setIdLocacao(locacao.getIdLocacao());
+        editado.setDtLocacao(locacao.getDtLocacao());
+        editado.setDtDevolucaoEfetiva(locacao.getDtDevolucaoEfetiva());
+        editado.setDtDevolucaoPrevista(locacao.getDtDevolucaoEfetiva());
+        editado.setMultaCobrada(locacao.getMultaCobrada());
+        editado.setValorCobrado(locacao.getValorCobrado());
+        editado.setItens(locacao.getItens());
+
+        return locacaoRepo.save(editado);
+
+    }
+
+    public List<Locacao> listAll() {
+        return locacaoRepo.findAll();
+    }
+
+    public Locacao listId(Long idLocacao) throws RelationTypeNotFoundException {
+        return locacaoRepo.findById(idLocacao)
+                .orElseThrow(() -> new RelationTypeNotFoundException("Locação não existe com id :" + idLocacao));
+    }
+
+    public void deleteId(Long idLocacao) throws RelationTypeNotFoundException {
+        Locacao locacao = locacaoRepo.findById(idLocacao)
+                .orElseThrow(() -> new RelationTypeNotFoundException("Locação não existe com id :" + idLocacao));
+        locacaoRepo.delete(locacao);
+    }
+}
