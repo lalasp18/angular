78b2654faa3fa9aa282693031971c1b56f472b87
lalasp18 <laspio18@gmail.com>
2023-11-29 11:38:40 -0300

    devolução

--- src/app/sprints/s3/listar/locacao-pendente/locacao-pendente.component.ts
+++ src/app/sprints/s3/listar/locacao-pendente/locacao-pendente.component.ts
@@ -18,7 +18,10 @@ export class LocacaoPendenteComponent implements OnInit, OnDestroy {
   unsubscribe$!: Subscription;
   locacaoParaDevolver!: Locacao;
   itemParaDevolverNome: string = "";
-  valorMulta: any;
+
+  valorMulta: number = 0;
+  valorMultaMessage: string = "";
+  dataEfet: any;
 
   staticAlertClosed = false;
   alertMessage: string | undefined;
@@ -28,18 +31,18 @@ export class LocacaoPendenteComponent implements OnInit, OnDestroy {
     private alertServ: AlertService,
     private locacaoService: LocacaoService,
     private router: Router
-  ) { }
+  ) { 
+  }
 
   ngOnInit() {
 
-let nom: Date = new Date(new Date().toISOString());
-
-console.log('format ',nom);
     this.unsubscribe$ = this.locacaoService.listarLocacaoPendente()
       .subscribe({
         next: (itens: any) => {
           const data = itens;
           this.locacaoData = data.sort((a: any, b: any) => (a.idLocacao < b.idLocacao) ? -1 : 1);
+          this.dataEfet = document.getElementById('efetDev') as HTMLInputElement;
+          this.dataEfet.valueAsDate = new Date();
         },
         error: (err: any) => {
           this.alertServ.warning('Dados não encontrados.')
@@ -76,46 +79,40 @@ console.log('format ',nom);
       next: (data: Locacao) => {
         this.locacaoParaDevolver = data;
         let dtAtual = new Date();
-        let outraDt = new Date(data.dtDevolucaoPrevista)
-        if (outraDt < dtAtual) {
-          let dias = this.diferencaEmDias(outraDt, dtAtual);
+        let locacaoIdDt = new Date(data.dtDevolucaoPrevista)
+        if (locacaoIdDt < dtAtual) {
+          let dias = this.diferencaEmDias(locacaoIdDt, dtAtual);
           this.valorMulta = dias * 12;
+          this.valorMultaMessage = "";
           this.locacaoParaDevolver.multaCobrada = this.valorMulta;
         } else {
-          this.valorMulta = "Este cliente não possui multa.";
+          this.valorMultaMessage = "Este cliente não possui multa.";
         }
       }
     })
   }
 
-  diferencaEmDias(outraData: Date, dataAtual: Date) {
+  diferencaEmDias(locacaoIdData: Date, dataAtual: Date) {
     const tempoAtual = dataAtual.getTime();
-    const tempoOutraData = outraData.getTime();
+    const tempolocacaoIdData = locacaoIdData.getTime();
 
-    const diferencaEmMilissegundos = tempoAtual - tempoOutraData;
+    const diferencaEmMilissegundos = tempoAtual - tempolocacaoIdData;
     const diferencaEmDias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
     return diferencaEmDias;
   }
-
-  formatarDataLocacao(){
-    this.locacaoParaDevolver.dtDevolucaoEfetiva = new Date();
-    const ano = this.locacaoParaDevolver.dtDevolucaoEfetiva.getFullYear();
-    const mes = (this.locacaoParaDevolver.dtDevolucaoEfetiva.getMonth() + 1).toString().padStart(2, '0');
-    const dia = this.locacaoParaDevolver.dtDevolucaoEfetiva.getDate().toString().padStart(2, '0');
-    const dataFormatada = `${ano}-${mes}-${dia}`;
-
-    console.log(dataFormatada);
-    this.locacaoParaDevolver.dtDevolucaoEfetiva = new Date(dataFormatada);
-    this.locacaoParaDevolver.dtDevolucaoPrevista = new Date(dataFormatada);
-  }
-
+  
   limparLocacaoParaDevolver() {
     this.locacaoParaDevolver = {} as Locacao;
     this.itemParaDevolverNome = "";
+    console.log(this.dataEfet)
   }
 
   devolverID() {
-    this.formatarDataLocacao();
+    let inputDate = document.getElementById('efetDev') as HTMLInputElement;
+    if(inputDate){
+      this.locacaoParaDevolver.dtDevolucaoEfetiva = this.dataEfet.value;
+      this.locacaoParaDevolver.dtDevolucaoPrevista = this.dataEfet.value;
+    }
     this.locacaoService.editarLocacao(this.locacaoParaDevolver).subscribe(data => {
       this.ngOnInit();
     });
